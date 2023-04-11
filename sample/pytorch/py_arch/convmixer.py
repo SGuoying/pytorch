74664852cdf824d1f9cb2d3f2cb4482b41a33eb6
SGuoying <826367045@qq.com>
2023-04-11 22:07:44 +0800

    add

--- sample/pytorch/py_arch/convmixer.py
+++ sample/pytorch/py_arch/convmixer.py
@@ -5,7 +5,7 @@ import torch.nn.functional as F
 from einops import repeat
 import pytorch_lightning as pl
 # from sample.pytorch_lightning.base import BaseModule
-from sample.pytorch.py_arch.base import BaseCfg, Residual, BaseModule
+from sample.pytorch.py_arch.base import BaseCfg, ConvMixerLayer, Residual, BaseModule
 
 from sample.pytorch.py_arch.bayes.core import log_bayesian_iteration
 
@@ -83,6 +83,55 @@ class ConvMixer(BaseModule):
         self.log(mode + "_accuracy", accuracy, prog_bar=True)
         return loss
 
+@dataclass
+class IsotropicCfg(BaseCfg):
+    hidden_dim: int = 128
+    kernel_size: int = 5
+    patch_size: int = 2
+    num_classes: int = 10
+
+    drop_rate: float = 0. 
+
+    
+class Isotropic(BaseModule):
+    def __init__(self, cfg: IsotropicCfg):
+        super().__init__(cfg)
+
+        self.layers = nn.ModuleList([
+            ConvMixerLayer(cfg.hidden_dim, cfg.kernel_size, cfg.drop_rate)
+            for _ in range(cfg.num_layers)
+        ])
+
+        self.embed = nn.Sequential(
+            nn.Conv2d(3, cfg.hidden_dim, kernel_size=cfg.patch_size, stride=cfg.patch_size),
+            nn.GELU(),
+            nn.BatchNorm2d(cfg.hidden_dim, eps=7e-5),  # eps>6.1e-5 to avoid nan in half precision
+        )
+        
+        self.digup = nn.Sequential(
+            nn.AdaptiveAvgPool2d((1,1)),
+            nn.Flatten(),
+            nn.Linear(cfg.hidden_dim, cfg.num_classes)
+        )
+
+        self.cfg = cfg
+        
+    def forward(self, x):
+        x = self.embed(x)
+        for layer in self.layers:
+            x = x + layer(x)
+        x = self.digup(x)
+        return x
+
+    def _step(self, batch, mode="train"):  # or "val"
+        input, target = batch
+        logits = self.forward(input)
+        loss = F.cross_entropy(logits, target)
+        self.log(mode + "_loss", loss, prog_bar=True)
+        accuracy = (logits.argmax(dim=1) == target).float().mean()
+        self.log(mode + "_accuracy", accuracy, prog_bar=True)
+        return loss
+    
 
 class SumConvMixer(ConvMixer):
     def __init__(self, cfg:ConvMixerCfg):
