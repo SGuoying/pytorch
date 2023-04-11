5a1e5c764ee7fec6a9539deacc8687f98d6242fc
SGuoying <826367045@qq.com>
2023-04-11 18:20:52 +0800

    update

--- sample/pytorch/py_arch/convmixer.py
+++ sample/pytorch/py_arch/convmixer.py
@@ -46,7 +46,7 @@ class ConvMixer(BaseModule):
                     nn.Conv2d(cfg.hidden_dim, cfg.hidden_dim, cfg.kernel_size, groups=cfg.hidden_dim, padding="same"),
                     nn.GELU(),
                     nn.BatchNorm2d(cfg.hidden_dim),
-                    SE(cfg.hidden_dim, cfg.squeeze_factor),
+                    # SE(cfg.hidden_dim, cfg.squeeze_factor),
                 )),
                 nn.Conv2d(cfg.hidden_dim, cfg.hidden_dim, kernel_size=1),
                 nn.GELU(),
@@ -168,38 +168,6 @@ class BayesConvMixer2(ConvMixer):
         accuracy = (log_posterior.argmax(dim=-1) == target).float().mean()
         self.log(mode + "_accuracy", accuracy, prog_bar=True)
         return loss
-    
-
-class BayesConvMixer(ConvMixer):
-    def __init__(self, cfg:ConvMixerCfg):
-        super().__init__(cfg)
-
-        log_prior = torch.zeros(1, cfg.num_classes)
-        self.register_buffer('log_prior', log_prior) 
-
-        self.cfg = cfg
-
-    def forward(self, x):
-        batch_size, _, _, _ = x.shape
-        log_prior = repeat(self.log_prior, '1 n -> b n', b=batch_size)
-
-        x = self.embed(x)
-        for layer in self.layers:
-            x = layer(x)
-            logits = self.digup(x) 
-            log_prior = log_bayesian_iteration(log_prior, logits)
-        
-        return log_prior
-
-    def _step(self, batch, mode="train"):  # or "val"
-        input, target = batch
-        log_posterior = self.forward(input)
-        loss = F.nll_loss(log_posterior, target)
-        self.log(mode + "_loss", loss, prog_bar=True)
-        accuracy = (log_posterior.argmax(dim=-1) == target).float().mean()
-        self.log(mode + "_accuracy", accuracy, prog_bar=True)
-        return loss
-
         
 class NormConvMixer(ConvMixer):
     def __init__(self, cfg:ConvMixerCfg):
