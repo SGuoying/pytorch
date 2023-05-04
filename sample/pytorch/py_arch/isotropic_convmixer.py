5d70b82d8204339d257bc81f931582579dac20f0
SGuoying <826367045@qq.com>
2023-05-04 20:52:23 +0800

    update

--- sample/pytorch/py_arch/isotropic_convmixer.py
+++ sample/pytorch/py_arch/isotropic_convmixer.py
@@ -4,7 +4,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from sample.pytorch.py_arch.base import BaseCfg, ConvMixerLayer, BaseModule, ConvMixerLayer2, ConvMixerLayer3
+from sample.pytorch.py_arch.base import BaseCfg, ConvMixerLayer, BaseModule, ConvMixerLayer2, ConvMixerLayer3, ConvMixerLayerSE
 from sample.pytorch.py_arch.convmixer import SE
 
 
@@ -73,6 +73,21 @@ class Isotropic2(Isotropic):
         x= self.digup(x)
         return x
 
+class IsotropicSE(Isotropic):
+    def __init__(self, cfg: IsotropicCfg):
+        super().__init__(cfg)
+        self.layers = nn.ModuleList([
+            # ConvMixerLayer(cfg.hidden_dim, cfg.kernel_size, cfg.drop_rate)
+            ConvMixerLayerSE(cfg.hidden_dim, cfg.kernel_size, cfg.drop_rate)
+            for _ in range(cfg.num_layers)
+        ])
+
+    def forward(self, x):
+        x = self.embed(x)
+        for layer in self.layers:
+            x = x + layer(x)
+        x = self.digup(x)
+        return x
 
 # %%
 class BayesIsotropic(Isotropic):
