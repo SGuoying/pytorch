7cf34991b854c3cdb1a97d1a5cf2eca1aa217164
SGuoying <826367045@qq.com>
2023-05-14 14:27:59 +0800

    cross_entry

--- sample/pytorch/py_arch/convmixer.py
+++ sample/pytorch/py_arch/convmixer.py
@@ -163,7 +163,7 @@ class BayesConvMixer2(ConvMixer):
     def _step(self, batch, mode="train"):  # or "val"
         input, target = batch
         log_posterior = self.forward(input)
-        loss = F.nll_loss(log_posterior, target)
+        loss = F.cross_entropy(log_posterior, target)
         self.log(mode + "_loss", loss, prog_bar=True)
         accuracy = (log_posterior.argmax(dim=-1) == target).float().mean()
         self.log(mode + "_accuracy", accuracy, prog_bar=True)
